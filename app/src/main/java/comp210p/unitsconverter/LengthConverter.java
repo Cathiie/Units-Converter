02a35498976cd10fc2309ad22eadd36982ad17fe
Nightwatch21 <xulian070608@gmail.com>
2016-03-22 16:21:56 +0000

    MainActvity layout created LengthConverter layout created Spinner set

--- app/src/main/java/comp210p/unitsconverter/LengthConverter.java
+++ app/src/main/java/comp210p/unitsconverter/LengthConverter.java
@@ -5,9 +5,24 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+
+
+import java.util.ArrayList;
+
 public class LengthConverter extends AppCompatActivity {
-    Spinner spinner01, spinner02, spinner03, spinner04, spinner05, spinner06, spinner07;
+    static Spinner spinner01;
+    static Spinner spinner02;
+    static Spinner spinner03;
+    static Spinner spinner04;
+    static Spinner spinner05;
+    static Spinner spinner06;
+    static Spinner spinner07;
     ArrayAdapter<CharSequence> adapter;
+
+    public static String UnitSymble;
+    public int UnitIndex;
+
+    public final double[] ConerterFactor = {0.001, 1, 10, 100, 1000, 0.0006213711922, 1.093613298, 3.280839895, 39.37007874};
 
 
     @Override
@@ -15,25 +30,64 @@ public class LengthConverter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_length_converter);
 
-        spinner01 = (Spinner)findViewById(R.id.spinner);
-        spinner02 = (Spinner)findViewById(R.id.spinner2);
-        spinner03 = (Spinner)findViewById(R.id.spinner3);
-        spinner04 = (Spinner)findViewById(R.id.spinner4);
-        spinner05 = (Spinner)findViewById(R.id.spinner5);
-        spinner06 = (Spinner)findViewById(R.id.spinner6);
-        spinner07 = (Spinner)findViewById(R.id.spinner7);
 
-        adapter = ArrayAdapter.createFromResource(this, R.array.UnitLength, android.R.layout.simple_spinner_item );
+        //set spinner list and layout
+        spinner01 = (Spinner) findViewById(R.id.spinner);
+        spinner02 = (Spinner) findViewById(R.id.spinner2);
+
+
+        adapter = ArrayAdapter.createFromResource(this, R.array.UnitLength, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner01.setAdapter(adapter);
         spinner02.setAdapter(adapter);
-        spinner03.setAdapter(adapter);
-        spinner04.setAdapter(adapter);
-        spinner05.setAdapter(adapter);
-        spinner06.setAdapter(adapter);
-        spinner07.setAdapter(adapter);
-
 
     }
+
+
+
+        //get spinner string
+
+    private int getIndex(String text) {
+
+        switch (text) {
+            case "Kilometer":
+                UnitIndex = 0;
+                break;
+            case "Meter":
+                UnitIndex = 1;
+                break;
+            case "Decimeter":
+                UnitIndex = 2;
+                break;
+            case "Centimeter":
+                UnitIndex = 3;
+                break;
+            case "Millimeter":
+                UnitIndex = 4;
+                break;
+            case "Mile":
+                UnitIndex = 5;
+                break;
+            case "Yard":
+                UnitIndex = 6;
+                break;
+            case "Feet":
+                UnitIndex = 7;
+                break;
+            case "Inch":
+                UnitIndex = 8;
+                break;
+        }
+        return UnitIndex;
+
+    }
+
+
+
+
 }
+
+
+
+
