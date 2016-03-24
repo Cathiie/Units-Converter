c30f095563787e2e25db88450f8b4fd0ec6bef05
Cathiie <c.pang@live.co.uk>
2016-03-24 08:15:15 +0000

    Update MassConverter.java

--- app/src/main/java/comp210p/unitsconverter/MassConverter.java
+++ app/src/main/java/comp210p/unitsconverter/MassConverter.java
@@ -1,47 +1,130 @@
 package comp210p.unitsconverter;
 //Created by Catherine
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
-
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
-public class MassConverter extends AppCompatActivity {
-    static Spinner spinner01;
-    static Spinner spinner02;
 
+public class MassConverter extends AppCompatActivity {
+    private static final DecimalFormat df = new DecimalFormat("#.000");
+
+    static Spinner spinner1;
+    static Spinner spinner2;
     ArrayAdapter<CharSequence> adapter;
 
-    public static String UnitSymbol;
-    public int UnitIndex;
+    public String firstUnit, secondUnit;
+    public int unitIndex,unitIndex1,unitIndex2;
+    public double inputNum,outputNum;
+    public TextView outputText=null,firstUnitSymbol, secondUnitSymbol;
 
-    public final double[] ConverterFactor = {0.06479891, 28.35, 453.59237, 0.907185, 0.001, 1000};
+
+    public final double[] ConverterFactor = {0.06479891, 28.35, 1, 453.59237, 0.907185, 0.001, 1000};
+    private final String[] unitSymbol = {"Grain","Ounce","Ton", "Grams", "Kilograms","Milligrams"};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mass_converter);
 
 
         //set spinner list and layout
-        spinner01 = (Spinner) findViewById(R.id.spinner);
-        spinner02 = (Spinner) findViewById(R.id.spinner2);
+        spinner1 = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
 
 
-        adapter = ArrayAdapter.createFromResource(this, R.array.UnitMass, android.R.layout.simple_spinner_item);
+        adapter = ArrayAdapter.createFromResource(this, R.array.unitMass, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner01.setAdapter(adapter);
-        spinner02.setAdapter(adapter);
+        spinner1.setAdapter(adapter);
+
+
+        spinner2.setAdapter(adapter);
+
+
+
+        //refer
+        outputText = (TextView) findViewById(R.id.textDisplay);
+        firstUnitSymbol = (TextView) findViewById(R.id.firstMass);
+        secondUnitSymbol = (TextView) findViewById(R.id.secondMass);
+
+
+        EditText amountEditText1 = (EditText) findViewById(R.id.editText);
+        amountEditText1.addTextChangedListener(amountEditTextWatcher);
 
     }
 
 
+
+
+
+
+
+
+    //
+    //get the value in inputText
+    //
+    private final TextWatcher amountEditTextWatcher = new TextWatcher() {
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count) {
+
+            try {
+                inputNum = Double.parseDouble(s.toString());
+                outputNum = calculate(unitIndex1, unitIndex2);
+                outputText.setText(String.valueOf(df.format(outputNum)));
+            }
+            catch (NumberFormatException e) {
+
+                outputText.setText("");
+
+            }
+
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) { }
+
+        @Override
+        public void beforeTextChanged(
+                CharSequence s, int start, int count, int after) { }
+    };
+
+    //----------------------------------------------
+    //Calculation for conversion
+    //---------------
+    private double calculate(int index1, int index2) {
+        double outputNum=0;
+
+        firstUnit = spinner1.getSelectedItem().toString();
+        unitIndex1 = getIndex(firstUnit);
+        firstUnitSymbol.setText(unitSymbol[unitIndex1]);
+        secondUnit = spinner2.getSelectedItem().toString();
+        unitIndex2 = getIndex(secondUnit);
+        secondUnitSymbol.setText(unitSymbol[unitIndex2]);
+
+        double factor1=ConverterFactor[index1];
+        double factor2=ConverterFactor[index2];
+        outputNum = inputNum/factor1*factor2;
+
+
+        return outputNum;
+    }
 
     //get spinner string
 
@@ -60,11 +143,14 @@ public class MassConverter extends AppCompatActivity {
             case "Ton":
                 UnitIndex = 3;
                 break;
-            case "Kilograms":
+            case "Grams":
                 UnitIndex = 4;
                 break;
-            case "Milligrams":
+            case "Kilograms":
                 UnitIndex = 5;
+                break;
+            case "Milligrams":
+                UnitIndex = 6;
                 break;
 
         }
